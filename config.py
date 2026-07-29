a11a2dc0a2c9bdbf5673a7ed191165c1878b84a3
Petr Novosad <petr.novosad.84@seznam.cz>
2026-07-29 12:48:30 +0200

    some styling

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 
 
 # rozměry
-width = 350
+width = 400
 height = 300
 
 main_color = "#ff8000"
-main_font = ("Arial", 14)
-button_font = ("Arial", 10)
+main_font = ("Helvetica", 14)
+button_font = ("Helvetica", 10)
 
 source_language = "en"
 target_language = "cs"
+languages = ["AUTO", "CS", "EN", "DE", "FR", "IT", "JA", "KO", "ES", "PT", "RU"]
 
 temp_dir = "temp"
 output_dir = "output"
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import filedialog
 from transcriber import translate_subtitles
 from config import *
+import customtkinter as ctk
 
 def open_file():
     file_path = filedialog.askopenfilename(
@@ -30,10 +31,6 @@ def transcribe():
     except Exception as e:
         progress_text.config(text=f"Chyba{e}")
 
-def exit_app():
-    window.destroy()
-
-
 
 # hlavní okno
 window = Tk()
@@ -42,40 +39,44 @@ window.minsize(width, height)
 window.resizable(width=False, height=False)
 window.config(bg=main_color)
 
-# texty
-input_text = Label(text="Input: ", font=main_font, bg=main_color, fg="black")
+# framy
+input_frame = Frame(window, bg=main_color)
+input_frame.pack()
+language_frame = Frame(window, bg=main_color)
+language_frame.pack(pady=15)
+other_frame = Frame(window, bg=main_color)
+other_frame.pack()
+
+# input frame
+input_text = Label(input_frame,text="Input: ", font=main_font, bg=main_color, fg="black")
 input_text.grid(column=0, row=0, padx=5, pady=5)
-progress_text = Label(text="", font=main_font, bg=main_color, fg="black")
-progress_text.grid(column=1, row=4, padx=5, pady=5)
-input_language = Label(text="Input language: ", font=main_font, bg=main_color, fg="black")
-input_language.grid(column=1, row=1, padx=5, pady=5)
-output_language = Label(text="Output language: ", font=main_font, bg=main_color, fg="black")
-output_language.grid(column=1, row=2, padx=5, pady=5)
-
 # vstup od uživatele
-srt_input = Entry(width=20, font=main_font)
+srt_input = Entry(input_frame,width=20, font=main_font)
 srt_input.grid(column=1, row=0, padx=5, pady=5)
+open_button = Button(input_frame,text="Open", font=button_font, command=open_file)
+open_button.grid(column=2, row=0, padx=10, pady=10)
 
-
-# tlačítka
-open_button = Button(text="Open", font=button_font, command=open_file)
-open_button.grid(column=2, row=0, padx=5, pady=5)
-transcribe_button = Button(text="Transcribe", font=main_font, command=transcribe)
-transcribe_button.grid(column=1, row=3, padx=5, pady=5)
-exit_button = Button(text="Exit", font=main_font, command=exit_app)
-exit_button.grid(column=1, row=5, padx=10, pady=10)
-
-# roletky
+# language frame
+input_language = Label(language_frame,text="Input language: ", font=main_font, bg=main_color, fg="black")
+input_language.grid(column=0, row=0, padx=5, pady=5)
+output_language = Label(language_frame,text="Output language: ", font=main_font, bg=main_color, fg="black")
+output_language.grid(column=0, row=1, padx=5, pady=5)
 input_lang_drop = StringVar(window)
 input_lang_drop.set("AUTO")
-input_lang_drop_options = OptionMenu(window, input_lang_drop, "AUTO", "EN", "CS", "DE", "FR", "IT", "JA", "KO", "ES", "PT", "RU")
-input_lang_drop_options.grid(column=2, row=1, padx=5, pady=5)
-
+input_lang_drop_options = OptionMenu(language_frame, input_lang_drop, *languages)
+input_lang_drop_options.grid(column=1, row=0, padx=5, pady=5)
 output_lang_drop = StringVar(window)
 output_lang_drop.set("CS")
-output_lang_drop_options = OptionMenu(window, output_lang_drop, "CS", "EN", "DE", "FR", "IT", "JA", "KO", "ES", "PT", "RU")
-output_lang_drop_options.grid(column=2, row=2, padx=5, pady=5)
+output_lang_drop_options = OptionMenu(language_frame, output_lang_drop, *languages[1:])
+output_lang_drop_options.grid(column=1, row=1, padx=5, pady=5)
 
+# other frame
+transcribe_button = Button(other_frame,text="Transcribe", font=main_font, command=transcribe)
+transcribe_button.pack(padx=5, pady=5)
+progress_text = Label(text="", font=main_font, bg=main_color, fg="black")
+progress_text.pack(padx=5, pady=5)
+exit_button = Button(text="Exit", font=main_font, command=window.destroy)
+exit_button.pack(padx=5, pady=5)
 
 
 window.mainloop()
